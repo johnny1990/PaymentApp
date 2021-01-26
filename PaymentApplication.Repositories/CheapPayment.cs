a5881d89c802b6d6c8db5d33157c708d11e76cd5
Man Ioan <man_ioan@live.com>
2021-01-27 00:38:50 +0200

    Implement CheapPayment type

---
+++ PaymentApplication.Repositories/CheapPayment.cs
@@ -0,0 +1,31 @@
+﻿using PaymentApplication.Contracts;
+using PaymentApplication.Database.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentApplication.Repositories
+{
+    public class CheapPayment : ICheapPayment
+    {
+        private PaymentDBEntities _db = new PaymentDBEntities();
+
+        public void SaveDataAsCheapPaymentService(CreditCard cd)
+        {
+            try
+            {
+                _db.CreditCards.Add(cd);
+                _db.SaveChanges();
+                Console.WriteLine("Cheap Payment has been processed successfully");
+
+                GC.SuppressFinalize(_db);
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+        }
+    }
+}
